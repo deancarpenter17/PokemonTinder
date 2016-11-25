52f1a11ac8d0ca828711b7e3a36239bf8e956da1
deancarpenter17 <deancarpenter17@gmail.com>
2016-11-25 15:01:24 -0600

    Completed Firebase/google login

--- app/src/main/java/com/carpenter/dean/pokemontinder/LoginActivity.java
+++ app/src/main/java/com/carpenter/dean/pokemontinder/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.carpenter.dean.pokemontinder;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -17,9 +18,6 @@ import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.fitness.data.Application;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
@@ -27,154 +25,121 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.gson.Gson;
+import com.google.firebase.database.ValueEventListener;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.Response;
+public class LoginActivity extends AppCompatActivity
+        implements GoogleApiClient.OnConnectionFailedListener {
 
-public class LoginActivity extends AppCompatActivity {
+    private final static int RC_SIGN_IN = 0;
+    private static final String TAG = "LoginActivity";
+    private boolean pressedSignIn = false;
 
-    public static final int RC_SIGN_IN = 0;
+    Button mCreateAccountButton;
+    SignInButton googleSignInButton;
+    ProgressDialog progressDialog;
 
-    Button mLoginButton;
-    SignInButton signInButton;
-    private ArrayList<Pokemon> mPokemonArrayList;
-    Map<String, Pokemon> users;
+    private User mUser;
+    Pokemon mPokemon;
 
-    Pokemon dbTest;
-    final FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference usersRef = database.getReference();
     GoogleApiClient mGoogleApiClient;
-    GoogleSignInAccount mGoogleSignInAccount;
     private FirebaseAuth mAuth;
+    private FirebaseUser mFirebaseUser;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private DatabaseReference databaseReference;
 
+    /**/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
-                    @Override
-                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-                        Toast.makeText(getApplicationContext(), "Failed to connect", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
-
+        databaseReference = FirebaseDatabase.getInstance().getReference();
         mAuth = FirebaseAuth.getInstance();
+
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    Toast.makeText(getApplicationContext(), "AUTH STATE CHANGED", Toast.LENGTH_SHORT).show();
-                    dbTest();
+                mFirebaseUser = firebaseAuth.getCurrentUser();
+                if (progressDialog != null)
+                    progressDialog.dismiss();
+                if (mFirebaseUser != null) {
+                    databaseReference.child("users").child(mFirebaseUser.getUid()).addListenerForSingleValueEvent(
+                            new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+                                    User user = dataSnapshot.getValue(User.class);
+                                    if (user == null && pressedSignIn) {
+                                        Toast.makeText(getApplicationContext(),
+                                                "ACCOUNT NOT FOUND", Toast.LENGTH_SHORT).show();
+                                        pressedSignIn = false;
+                                    }
+
+                                    if (user != null) {
+                                        Log.d(TAG, "" + user.getName() + ": " + user.getPokemon().getName());
+                                        Intent intent = MainActivity.newIntent(getApplicationContext(),
+                                                mUser);
+                                        startActivity(intent);
+
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
+
+                                }
+                            }
+                    );
+                    Log.d("onAuthStateChanged", "signed_in:" + mFirebaseUser.getUid());
                 } else {
                     // User is signed out
+                    Log.d("", "onAuthStateChanged:signed_out");
                 }
                 // ...
             }
         };
 
-        signInButton = (SignInButton) findViewById(R.id.google_sign_in_button);
-        signInButton.setSize(SignInButton.SIZE_STANDARD);
-        signInButton.setOnClickListener(new View.OnClickListener() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+
+        googleSignInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        googleSignInButton.setSize(SignInButton.SIZE_STANDARD);
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                mGoogleApiClient.clearDefaultAccountAndReconnect();
-                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-                startActivityForResult(signInIntent, RC_SIGN_IN);
+            public void onClick(View v) {
+                progressDialog = ProgressDialog.show(LoginActivity.this, "Turning the wrenches...",
+                        "Loading...", true);
+                signIn();
+                pressedSignIn = true;
             }
         });
 
-        mPokemonArrayList = new ArrayList<>();
-        users = new HashMap<>();
-
-        mLoginButton = (Button) findViewById(R.id.login_button);
-        mLoginButton.setOnClickListener(new View.OnClickListener() {
+        mCreateAccountButton = (Button) findViewById(R.id.create_account_button);
+        mCreateAccountButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = SwipeActivity.newIntent(getApplicationContext(), mPokemonArrayList);
-                startActivity(i);
-            }
-        });
-
-        new PokemonDownloader().getPokemon(5, new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-
-                final Gson gson = new Gson();
-
-                try {
-                    Pokemon pokemon = gson.fromJson(response.body().charStream(), Pokemon.class);
-                    mPokemonArrayList.add(pokemon);
-                    Log.d("POKEMON CREATED: ", pokemon.getName() + ", URL: " + pokemon.getPokemonUrl().getUrl() +
-                            ", Pic URL: " + pokemon.getSprites().getFrontDefault());
-                    Log.d("IN ALIST", ""+mPokemonArrayList.size());
-
-                    if (mPokemonArrayList.size() > 4) {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                mLoginButton.setVisibility(View.VISIBLE);
-                            }
-                        });
-                    }
-                } catch (Exception err) {
-                    Log.e("ERR", err.toString());
-                }
+                startActivity(new Intent(getApplicationContext(), CreateAccountActivity.class));
             }
         });
     }
 
-    public void dbTest() {
-
-        if(mAuth != null) {
-
-            new PokemonDownloader().getPokemon(1, new Callback() {
-                @Override
-                public void onFailure(Call call, IOException e) {
-
-                }
-
-                @Override
-                public void onResponse(Call call, Response response) throws IOException {
-                    final Gson gson = new Gson();
-
-                    try {
-                        dbTest = gson.fromJson(response.body().charStream(), Pokemon.class);
-                        Log.d("DB POKEMON CREATED: ", dbTest.getName() + ", URL: " + dbTest.getPokemonUrl().getUrl() +
-                                ", Pic URL: " + dbTest.getSprites().getFrontDefault());
-                        DatabaseReference reference = usersRef.child("users").child(mAuth.getCurrentUser().getUid());
-                        reference.setValue(dbTest);
-
-                    } catch (Exception err) {
-                        Log.e("ERR", err.toString());
-                    }
-                }
-            });
-        }
+    private void signIn() {
+        mGoogleApiClient.clearDefaultAccountAndReconnect();
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
     @Override
@@ -189,29 +154,35 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void handleSignInResult(GoogleSignInResult result) {
+        Log.d("handleSignInResult", "handleSignInResult:" + result.isSuccess());
         if (result.isSuccess()) {
             // Signed in successfully, show authenticated UI.
-            mGoogleSignInAccount = result.getSignInAccount();
-            firebaseAuthWithGoogle(mGoogleSignInAccount);
+            GoogleSignInAccount acct = result.getSignInAccount();
+            firebaseAuthWithGoogle(acct);
+        } else {
+            // Signed out, show unauthenticated UI.
         }
     }
 
     private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d("AUTH", "firebaseAuthWithGoogle:" + acct.getId());
 
         AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
         mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("AUTH", "signInWithCredential:onComplete:" + task.isSuccessful());
 
-                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // If sign in fails, display a message to the mFirebaseUser. If sign in succeeds
                         // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
+                        // signed in mFirebaseUser can be handled in the listener.
                         if (!task.isSuccessful()) {
+                            Log.w("AUTH", "signInWithCredential", task.getException());
                             Toast.makeText(LoginActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                         }
-
+                        // ...
                     }
                 });
     }
@@ -230,14 +201,9 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-    private void signOut() {
-        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
-                new ResultCallback<Status>() {
-                    @Override
-                    public void onResult(Status status) {
-                        // ...
-                    }
-                });
-    }
 
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
