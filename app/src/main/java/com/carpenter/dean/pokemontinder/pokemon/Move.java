b32cd603151318dd9a029170433bb1c641561383
deancarpenter17 <deancarpenter17@gmail.com>
2017-01-05 21:23:08 -0600

    added pokemon moves

--- app/src/main/java/com/carpenter/dean/pokemontinder/pokemon/Move.java
+++ app/src/main/java/com/carpenter/dean/pokemontinder/pokemon/Move.java
@@ -1,8 +1,50 @@
 package com.carpenter.dean.pokemontinder.pokemon;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by deanc on 12/29/2016.
  */
 
-public class Move {
+public class Move implements Parcelable {
+
+    private MoveDetails move;
+
+    public Move() {}
+
+    public MoveDetails getMove() {
+        return move;
+    }
+
+    public void setMove(MoveDetails move) {
+        this.move = move;
+    }
+
+    protected Move(Parcel in) {
+        move = (MoveDetails) in.readValue(MoveDetails.class.getClassLoader());
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(move);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Move> CREATOR = new Parcelable.Creator<Move>() {
+        @Override
+        public Move createFromParcel(Parcel in) {
+            return new Move(in);
+        }
+
+        @Override
+        public Move[] newArray(int size) {
+            return new Move[size];
+        }
+    };
 }
--- app/src/main/java/com/carpenter/dean/pokemontinder/pokemon/MoveDetails.java
+++ app/src/main/java/com/carpenter/dean/pokemontinder/pokemon/MoveDetails.java
@@ -1,11 +1,18 @@
 package com.carpenter.dean.pokemontinder.pokemon;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by deanc on 12/29/2016.
  */
 
-public class MoveDetails {
+public class MoveDetails implements Parcelable {
+
     private String name;
+    private String url;
+
+    public MoveDetails() {}
 
     public String getName() {
         return name;
@@ -14,4 +21,41 @@ public class MoveDetails {
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    protected MoveDetails(Parcel in) {
+        name = in.readString();
+        url = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(url);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<MoveDetails> CREATOR = new Parcelable.Creator<MoveDetails>() {
+        @Override
+        public MoveDetails createFromParcel(Parcel in) {
+            return new MoveDetails(in);
+        }
+
+        @Override
+        public MoveDetails[] newArray(int size) {
+            return new MoveDetails[size];
+        }
+    };
 }
