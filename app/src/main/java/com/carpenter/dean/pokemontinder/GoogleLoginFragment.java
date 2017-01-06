92fb748cc88752433303a38b31e7644c6426fd32
deancarpenter17 <deancarpenter17@gmail.com>
2017-01-05 20:46:04 -0600

    finished implementing G+ sign in

--- app/src/main/java/com/carpenter/dean/pokemontinder/GoogleLoginFragment.java
+++ app/src/main/java/com/carpenter/dean/pokemontinder/GoogleLoginFragment.java
@@ -1,58 +1,224 @@
 package com.carpenter.dean.pokemontinder;
 
+import android.app.ProgressDialog;
 import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.carpenter.dean.pokemontinder.pokemon.Pokemon;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link GoogleLoginFragment.OnFragmentInteractionListener} interface
+ * {@link Callbacks} interface
  * to handle interaction events.
  */
-public class GoogleLoginFragment extends Fragment {
+public class GoogleLoginFragment extends Fragment
+        implements GoogleApiClient.OnConnectionFailedListener {
 
-    private OnFragmentInteractionListener mListener;
+    private final static int RC_SIGN_IN = 0;
+    private final static String USER = "user";
+    private final static String TAG = "GoogleLoginFragment";
 
-    public GoogleLoginFragment() {
-        // Required empty public constructor
+    private Pokemon mPokemon;
+
+    private User mUser;
+
+    private SignInButton mSignInButton;
+    private Button mNextButton;
+    private ProgressDialog progressDialog;
+
+    private DatabaseReference mDatabase;
+    private GoogleApiClient mGoogleApiClient;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mFirebaseUser;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private Callbacks mCallbacks;
+
+    public static Fragment newInstance() {
+        return new GoogleLoginFragment();
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_google_login, container, false);
+        View v = inflater.inflate(R.layout.fragment_google_login, container, false);
+
+        mUser = new User();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                mFirebaseUser = firebaseAuth.getCurrentUser();
+                if(mFirebaseUser != null) {
+                    mUser.setUuid(mFirebaseUser.getUid());
+                    // only want to store first name in database
+                    int i = mFirebaseUser.getDisplayName().indexOf(' ');
+                    String name = mFirebaseUser.getDisplayName().substring(0, i);
+                    mUser.setName(name);
+                    Log.d("onAuthStateChanged", "signed_in:" + mFirebaseUser.getUid());
+                    Toast.makeText(getContext(), mFirebaseUser.getDisplayName() + " signed in!",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    // User is signed out
+                    Log.d("", "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                .enableAutoManage(getActivity() /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        mSignInButton = (SignInButton) v.findViewById(R.id.create_account_sign_in_button);
+        mSignInButton.setSize(SignInButton.SIZE_STANDARD);
+        v.findViewById(R.id.create_account_sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mGoogleApiClient.clearDefaultAccountAndReconnect();
+                signIn();
+            }
+        });
+
+        mNextButton = (Button) v.findViewById(R.id.login_fragment_next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mFirebaseUser == null) {
+                    Toast.makeText(getContext(), "MUST SIGN IN WITH GOOGLE!", Toast.LENGTH_LONG).show();
+                }
+                else if(mUser != null) {
+                    mCallbacks.switchToPokemonChooserFrag(mUser);
+                }
+                else {
+                    Toast.makeText(getContext(), "Error logging in!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        return v;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if(mListener != null) {
-            mListener.onFragmentInteraction(uri);
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if(requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d("handleSignInResult", "handleSignInResult:" + result.isSuccess());
+        if(result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            firebaseAuthWithGoogle(acct);
+        } else {
+            // Signed out, show unauthenticated UI.
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d("AUTH", "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("AUTH", "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the mFirebaseUser. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in mFirebaseUser can be handled in the listener.
+                        if(!task.isSuccessful()) {
+                            Log.w("AUTH", "signInWithCredential", task.getException());
+
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e(TAG, connectionResult.getErrorMessage());
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if(mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
         }
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if(context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
+        if(context instanceof Callbacks) {
+            mCallbacks = (Callbacks) context;
         } else {
             throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+                    + " must implement Callbacks");
         }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
+        mCallbacks = null;
     }
 
     /**
@@ -65,8 +231,10 @@ public class GoogleLoginFragment extends Fragment {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnFragmentInteractionListener {
+    public interface Callbacks {
         // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        public void switchToPokemonChooserFrag(User user);
     }
 }
+
+
