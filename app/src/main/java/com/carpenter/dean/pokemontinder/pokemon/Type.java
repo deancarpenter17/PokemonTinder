11f8a453df7b9cbf043624f675cb02dd1337d10f
deancarpenter17 <deancarpenter17@gmail.com>
2017-01-05 21:24:11 -0600

    added attribute: type of pokemon (grass, rock, etc.)

--- app/src/main/java/com/carpenter/dean/pokemontinder/pokemon/Type.java
+++ app/src/main/java/com/carpenter/dean/pokemontinder/pokemon/Type.java
@@ -1,12 +1,17 @@
 package com.carpenter.dean.pokemontinder.pokemon;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by deanc on 12/29/2016.
  */
 
-public class Type {
+public class Type implements Parcelable {
     transient Integer slot;
     private TypeDetails type;
+
+    public Type() {}
 
     public TypeDetails getType() {
         return type;
@@ -15,4 +20,39 @@ public class Type {
     public void setType(TypeDetails type) {
         this.type = type;
     }
+
+    protected Type(Parcel in) {
+        slot = in.readByte() == 0x00 ? null : in.readInt();
+        type = (TypeDetails) in.readValue(TypeDetails.class.getClassLoader());
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        if (slot == null) {
+            dest.writeByte((byte) (0x00));
+        } else {
+            dest.writeByte((byte) (0x01));
+            dest.writeInt(slot);
+        }
+        dest.writeValue(type);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Type> CREATOR = new Parcelable.Creator<Type>() {
+        @Override
+        public Type createFromParcel(Parcel in) {
+            return new Type(in);
+        }
+
+        @Override
+        public Type[] newArray(int size) {
+            return new Type[size];
+        }
+    };
 }
+
--- app/src/main/java/com/carpenter/dean/pokemontinder/pokemon/TypeDetails.java
+++ app/src/main/java/com/carpenter/dean/pokemontinder/pokemon/TypeDetails.java
@@ -1,13 +1,18 @@
 package com.carpenter.dean.pokemontinder.pokemon;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by deanc on 12/29/2016.
  */
 
-public class TypeDetails {
+public class TypeDetails implements Parcelable {
 
     private String name;
     private String url;
+
+    public TypeDetails() {}
 
     public String getName() {
         return name;
@@ -24,4 +29,33 @@ public class TypeDetails {
     public void setUrl(String url) {
         this.url = url;
     }
+
+    protected TypeDetails(Parcel in) {
+        name = in.readString();
+        url = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(url);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<TypeDetails> CREATOR = new Parcelable.Creator<TypeDetails>() {
+        @Override
+        public TypeDetails createFromParcel(Parcel in) {
+            return new TypeDetails(in);
+        }
+
+        @Override
+        public TypeDetails[] newArray(int size) {
+            return new TypeDetails[size];
+        }
+    };
 }
